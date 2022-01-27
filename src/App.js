54b24ab8f2e502a43d135a358c4aee5445ab417d
Ganeshkumar M <ganeshkumar29401@gmail.com>
2022-01-27 15:30:09 +0530

    router's navlink added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, useCallback } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Auth from "./components/Auth/auth";
 import Blog from "./components/Blog/Blog";
 import Header from "./components/Navigation/Header";
+import BlogForm from "./components/Blog/BlogForm";
 function App() {
   const [isFormShown, setIsFormShown] = useState(false);
   const [isLoginState, setIsLoginState] = useState(false);
-  const openAuthForm = (action= 'SignIn') => {
+  const openAuthForm = useCallback((action = "SignIn") => {
     action === "SignIn" ? setIsLoginState(true) : setIsLoginState(false);
     setIsFormShown(true);
-  };
+  }, []);
   const closeAuthForm = () => {
     setIsFormShown(false);
   };
 
-  const changeLoginStateHandler = ()=>{
-    setIsLoginState((prevState) => !prevState)
-  }
+  const changeLoginStateHandler = () => {
+    setIsLoginState((prevState) => !prevState);
+  };
 
-  useEffect(() => {
-    const timer = setTimeout(() => openAuthForm(), 3000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => openAuthForm(), 3000);
+  //   return () => {
+  //     clearTimeout(timer);
+  //   };
+  // }, [openAuthForm]);
 
   return (
     <Fragment>
       <Header heading="Trending Blog" onAction={openAuthForm} />
-      <Blog />
+      <Routes>
+        <Route path="/" element={<Navigate to="/blogs" />} />
+        <Route path="/blogs" element={<Blog />} />
+        <Route path="/addblogs" element={<BlogForm />} />
+      </Routes>
       <section>
         {isFormShown && (
           <Auth
--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Header.module.css";
 const Header = (props) => {
   return (
     <header className={classes.header}>
       <h2>{props.heading}</h2>
+      <div className={classes.navigation}>
+        <NavLink className={({isActive}) => (isActive ? classes.active : "")} to='/blogs'>
+          Blogs
+        </NavLink>
+        <NavLink className={({isActive}) => (isActive ? classes.active : "")} to='/addblogs'>
+          Add Blogs
+        </NavLink>
+      </div>
       <div className={classes.actions}>
-        <button type="button" onClick={() => props.onAction('SignIn')}>
+        <button type="button" onClick={() => props.onAction("SignIn")}>
           Log in
         </button>
-        <button type="button" onClick={() => props.onAction('SignUp')}>
+        <button type="button" onClick={() => props.onAction("SignUp")}>
           Sign up
         </button>
       </div>
