6da734d5692282638d6b52dcaa4085d4e4286d08
Ganeshkumar M <ganeshkumar29401@gmail.com>
2022-01-27 19:38:47 +0530

    Added form validation to blogform

--- src/components/Blog/BlogForm.js
+++ src/components/Blog/BlogForm.js
@@ -1,22 +1,97 @@
+import useInput from "../../hooks/use-input";
 import classes from "./BlogForm.module.css";
 const BlogForm = (props) => {
+  let formIsValid = false;
+  const {
+    value: enteredTitle,
+    isValid: titleIsValid,
+    isInvalid: titleIsInvalid,
+    BlurHandler: titleBlurHandler,
+    Changehandler: titleChangeHandler,
+    resetHandler: titleResetHandler,
+  } = useInput((val) => val.trim().length !== 0);
+
+  const {
+    value: enteredAuthor,
+    isValid: authorIsValid,
+    isInvalid: authorIsInvalid,
+    BlurHandler: authorBlurHandler,
+    Changehandler: authorChangeHandler,
+    resetHandler: authorResetHandler,
+  } = useInput((val) => val.trim().length !== 0);
+
+  const {
+    value: enteredDes,
+    isValid: desIsValid,
+    isInvalid: desIsInvalid,
+    BlurHandler: desBlurHandler,
+    Changehandler: desChangeHandler,
+    resetHandler: desResetHandler,
+  } = useInput((val) => val.trim().length !== 0);
+
+  if(titleIsValid && authorIsValid && desIsValid){
+    formIsValid=true;
+  }
+
+  const formClickHandler = (event) => {
+    event.preventDefault();
+    if (!titleIsValid && !authorIsValid && !desIsValid) {
+      return;
+    }
+    console.log({
+      title:enteredTitle,
+      des:enteredDes,
+      author:enteredAuthor,
+    });
+    titleResetHandler();
+    authorResetHandler();
+    desResetHandler();
+  };
   return (
-    <div className={classes.form}>
-    <div className={classes.formWrapper}>
-      <div className={classes.fields}>
-        <label htmlFor="title">Title</label>
-        <input type="text" id="title" />
-        <label htmlFor="author">Author</label>
-        <input type="text" id="author" />
-        <label htmlFor="des">Description</label>
-        <textarea type="text" id="des" />
-      </div>
-      <div className={classes.form_actions}>
-        <button type="button">Add</button>
-        <button type="button">Cancel</button>
-      </div>
-    </div>
-    </div>
+      <form className={classes.formWrapper}>
+        <div className={classes.fields}>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onBlur={titleBlurHandler}
+            onChange={titleChangeHandler}
+            id="title"
+          />
+          {titleIsInvalid && <p className={classes.error_text}>Title can't be empty</p>}
+          <label htmlFor="author">Author</label>
+          <input
+            type="text"
+            value={enteredAuthor}
+            onChange={authorChangeHandler}
+            onBlur={authorBlurHandler}
+            id="author"
+          />
+          {authorIsInvalid && <p className={classes.error_text}>Author can't be empty</p>}
+          <label htmlFor="des">Description</label>
+          <textarea
+            type="text"
+            id="des"
+            onChange={desChangeHandler}
+            value={enteredDes}
+            onBlur={desBlurHandler}
+          />
+          {desIsInvalid && <p className={classes.error_text}>Description can't be empty</p>}
+        </div>
+        <div className={classes.form_actions}>
+          <button onClick={formClickHandler} disabled={!formIsValid}>Add</button>
+          <button
+            type="button"
+            onClick={() => {
+              titleResetHandler();
+              authorResetHandler();
+              desResetHandler();
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      </form>
   );
 };
 
